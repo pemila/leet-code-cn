e460f1dcae868be88b56fe9f8c84367f2ce79b60
pemila <pemila@126.com>
2019-06-19 11:55:14 +0800

    add 组合总和 and 最大交换

--- src/main/java/com/pemila/leetcode/CombinationSum.java
+++ src/main/java/com/pemila/leetcode/CombinationSum.java
@@ -1,8 +1,61 @@
 package com.pemila.leetcode;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @author 月在未央
  * @date 2019/6/18 15:42
  */
 public class CombinationSum {
+    /*
+        216 组合总和3
+            找出所有相加之和为 n 的 k 个数的组合。组合中只允许含有 1 - 9 的正整数，并且每种组合中不存在重复的数字。
+        说明：
+            所有数字都是正整数。
+            解集不能包含重复的组合。 
+        示例 1:
+            输入: k = 3, n = 7
+            输出: [[1,2,4]]
+        示例 2:
+            输入: k = 3, n = 9
+            输出: [[1,2,6], [1,3,5], [2,3,4]]
+    */
+
+
+    /**
+     解题思路：
+        使用递归
+        1. 循环 1-9 依次加入 LinkedList
+        2. 每次循环 k-1(LinkedList剩余数量)，n-i（n的剩余大小）
+        3. k=0 && n==0 时，表示 已加入LinkedList中的 k 个数的和为 n
+        4. n!=0或者k=0的情况下，将LinkedList的最后一个元素移除
+     */
+
+    private List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> list = new ArrayList<>();
+        traceBack(list,new LinkedList<>(),k,n,1);
+        return list;
+    }
+
+    private void traceBack(List<List<Integer>> res, LinkedList<Integer> list, int k, int n, int start) {
+        if (n == 0 && k == 0) {
+            res.add(new ArrayList<>(list));
+        } else if (n > 0 && k > 0) {
+            for (int i = start; i <= 9; i++) {
+                list.add(i);
+                traceBack(res, list, k - 1, n - i, i + 1);
+                list.removeLast();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        CombinationSum sum = new CombinationSum();
+        int k = 3;
+        int n = 15;
+        List<List<Integer>> lists = sum.combinationSum3(k,n);
+        System.out.println(lists.toString());
+    }
 }
--- src/main/java/com/pemila/leetcode/MaximumSwap.java
+++ src/main/java/com/pemila/leetcode/MaximumSwap.java
@@ -1,8 +1,86 @@
 package com.pemila.leetcode;
 
+import java.util.Arrays;
+
 /**
  * @author 月在未央
  * @date 2019/6/18 16:55
  */
 public class MaximumSwap {
+
+    /*
+        670 最大交换
+            给定一个非负整数，你至多可以交换一次数字中的任意两位。返回你能得到的最大值。
+        示例 1 :
+            输入: 2736
+            输出: 7236
+            解释: 交换数字2和数字7。
+        示例 2 :
+            输入: 9973
+            输出: 9973
+            解释: 不需要交换。
+        注意:
+            给定数字的范围是 [0, 10E8]
+    */
+
+    /*
+        解题思路
+            要求获得最大交换，
+            首先确定给定的数字序列可排序成的最大数，
+            然后使用给定数字与最大数按位进行比较，
+            一旦某位上的数字不一致，则说明交换该位上的数字即可实现最大交换
+
+            例如：
+               给定数字 2736
+               从大到小排序可得 7263
+               使用2736与7263按位比较，
+               首位数字不同，则说明要交换给定数字2736中的2与7.
+               交换后可得 7236，即输出7236
+     */
+
+
+    private int maximumSwap(int num) {
+        String numStr = String.valueOf(num);
+        char[] array = numStr.toCharArray();
+
+        // 对给定数字序列重排序从小到大
+        char[] newChars = numStr.toCharArray();
+        Arrays.sort(newChars);
+
+        int length = numStr.length();
+
+        // 找到需要交换的第一个数字
+        int index = -1;
+        for(int i=0;i<length;i++){
+            if(array[i] != newChars[length-i-1]){
+                index = i;
+                break;
+            }
+        }
+        if(index >0 ){
+            // 查找需要交换的第二个数字
+            for(int i = length -1;i>=index;i--){
+                if(array[i] == newChars[length -1 -index]){
+                    swap(array,index,i);
+                    break;
+                }
+            }
+            return Integer.parseInt(new String(array));
+        } else {
+            return num;
+        }
+    }
+
+    private void swap(char[] chars, int lo, int hi) {
+        char tmp = chars[lo];
+        chars[lo] = chars[hi];
+        chars[hi] = tmp;
+    }
+
+    public static void main(String[] args) {
+        MaximumSwap ms = new MaximumSwap();
+        int num = 7564154;
+        System.out.println(ms.maximumSwap(num));
+
+    }
 }
